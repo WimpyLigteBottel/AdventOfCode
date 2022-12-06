0735d898441478bff7fc5f6a6e2dbfa199e2a3f3
Marco-PC\Marco <epossmurfmarco@gmail.com>
2022-12-06 12:22:15 +0100

    day 6 solved

--- 2022/src/main/kotlin/nel/marco/Day6.kt
+++ 2022/src/main/kotlin/nel/marco/Day6.kt
@@ -1,7 +1,5 @@
 package nel.marco
 
-import java.util.*
-
 
 class Day6(var readInput: List<String>) {
 
@@ -10,8 +8,10 @@ class Day6(var readInput: List<String>) {
     }
 
     fun process(input: String, distinct: Int = 4): Int {
-        for (x in 0..input.length - distinct) {
-            if (input.substring(x, x + distinct).toSet().size == distinct) {
+        val maxLength = input.length - distinct
+        for (x in 0..maxLength) {
+            val substringUnique = input.substring(x, x + distinct).toSet()
+            if (substringUnique.size == distinct) {
                 return x + distinct
             }
         }
@@ -20,17 +20,13 @@ class Day6(var readInput: List<String>) {
 
     fun answerOne(): String {
         val buffer = readInput.get(0)
-
-
         return process(buffer).toString()
     }
 
     fun answerTwo(): String {
         val buffer = readInput.get(0)
         return process(buffer, 14).toString()
-
     }
-
 
 }
 
@@ -38,11 +34,11 @@ fun main(args: Array<String>) {
     var readAllLines = MarcoUtil.readInput(Day6.day, false) as MutableList<String>
 
     val part1: () -> Unit = {
-        val day = Day5(readAllLines)
+        val day = Day6(readAllLines)
         println("part1 = ${day.answerOne()}")
     }
     val part2: () -> Unit = {
-        val day = Day5(readAllLines)
+        val day = Day6(readAllLines)
         println("part2 = ${day.answerTwo()}")
     }
     MarcoUtil.time("part1", part1)
