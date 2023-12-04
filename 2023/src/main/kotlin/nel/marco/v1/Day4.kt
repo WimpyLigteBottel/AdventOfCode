8fdcac422e0f465bc09ce1b90f83224c1f1f02d6
mnel <mnel@bol.com>
2023-12-04 12:14:54 +0100

    Refactoring day 4

--- 2023/src/main/kotlin/nel/marco/v1/Day4.kt
+++ 2023/src/main/kotlin/nel/marco/v1/Day4.kt
@@ -19,56 +19,63 @@ class Day4(var readInput: List<String>) {
 
     fun answerOne(): String {
 
-        return readInput.map {
-            val afterGame = it.split(":")[1]
-            val winningNumbers = extractNumbers(afterGame, 0).toList()
-            val firstSet = extractNumbers(afterGame, 1).toList()
-
-            winningNumbers.intersect(firstSet).count()
-
-        }
-            .filter { it > 0 } // only winning counts
-            .map { Math.pow(2.0, it.toDouble() - 1).toInt() } // calculate actual points
-            .sum()
+        return readInput
+            .map { it.split(":")[1] } // Get everything after ":"
+            .map { it.extractWinningAndActualNumberPair() } // get winning and actual number pair
+            .map { it.intersect().count() } // intersect and count the amount of winning numbers
+            .filter { it > 0 } // only wining numbers
+            .sumOf { Math.pow(2.0, it.toDouble() - 1).toInt() } // calculate points and sum it
             .toString()
     }
 
-    private fun extractNumbers(afterGame: String, side: Int = 0) =
-        "\\d+".toRegex().findAll(afterGame.split("|")[side]).map {
-            it.value
-        }.toList()
+    private fun extractWinningAndActualNumberPair(afterGame: String, side: Int = 0) =
+        "\\d+".toRegex()
+            .findAll(afterGame.split("|")[side])
+            .map { it.value }
+            .toList()
+
+    private fun String.extractWinningAndActualNumberPair(): Pair<List<String>, List<String>> {
+        val winningNumbers = extractWinningAndActualNumberPair(this, 0)
+        val actualNumbers = extractWinningAndActualNumberPair(this, 1)
+        return Pair(winningNumbers, actualNumbers)
+    }
+
+    private fun Pair<List<String>, List<String>>.intersect() = this.second.intersect(this.first)
 
     fun answerTwo(): String {
-
         var copies = readInput.toMutableList()
         var mapCounter = mutableMapOf<String, Long>()
+        var uniqueSet = copies.toList()
         while (copies.isNotEmpty()) {
-            var it = copies[0]
-            val gameNumber = it.split(":")[0]
-            mapCounter.putIfAbsent(gameNumber, 0)
-            val winningNumbers = extractNumbers(it.split(":")[1], 0).toList()
-            val firstSet = extractNumbers(it.split(":")[1], 1).toList()
-            val matching = winningNumbers.intersect(firstSet).toList().count()
 
-            val countOfCopies = copies.count { it == copies[0] }
+            val timesWon = copies[0].split(":")[1]
+                .extractWinningAndActualNumberPair()
+                .intersect()
+                .count()
+
+            val countOfCopies = copies.count { x -> x.startsWith(copies[0]) }
+
+            val gameNumber = copies[0].split(":")[0]
+            mapCounter.putIfAbsent(gameNumber, 0)
+            mapCounter[gameNumber] = mapCounter[gameNumber]!! + countOfCopies
+
+
             runCatching {
                 var tempList = mutableListOf<String>()
-                val uniqueSet = copies.toSet().toList()
-                for (x in 1..matching) {
-                    val element = uniqueSet[x]
-                    tempList.add(element)
+                for (x in 1..timesWon) {
+                    tempList.add(uniqueSet[x])
                 }
-
                 for (x in 1..countOfCopies) {
                     copies.addAll(tempList)
                 }
+                // remove all the duplicates because they have been processed
+                copies.removeIf { it == copies[0] }
+
+                // sort the remaining once's
                 copies.sort()
             }
-
-
-            mapCounter[gameNumber] = mapCounter[gameNumber]!! + countOfCopies
-
-            copies.removeIf { it == copies[0] }
+            // remove the first element in the unique set
+            uniqueSet = uniqueSet.subList(1, uniqueSet.size)
         }
 
         return mapCounter.values.sum().toString()
