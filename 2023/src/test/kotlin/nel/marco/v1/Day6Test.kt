4a6f9ce3c5c83918a5fd87891eed998f034babd7
mnel <mnel@bol.com>
2023-12-07 16:26:29 +0100

    Fixing old day tests

--- 2023/src/test/kotlin/nel/marco/v1/Day6Test.kt
+++ 2023/src/test/kotlin/nel/marco/v1/Day6Test.kt
@@ -13,10 +13,10 @@ class Day6Test {
     @Test
     fun answerOne() {
         val readInputExample = ReadUtil.readInputAsList(dayNumber, true, true)
-        assertEquals("288", Day7(readInputExample).answerOne())
+        assertEquals("288", Day6(readInputExample).answerOne())
 
         val real = ReadUtil.readInputAsList(dayNumber, false, true)
-        assertEquals("345015", Day7(real).answerOne())
+        assertEquals("345015", Day6(real).answerOne())
 
     }
 
@@ -24,10 +24,10 @@ class Day6Test {
     fun answerTwo() {
 
         val readInputExample = ReadUtil.readInputAsList(dayNumber, true, true)
-        assertEquals("71503", Day7(readInputExample).answerTwo())
+        assertEquals("71503", Day6(readInputExample).answerTwo())
 
         val real = ReadUtil.readInputAsList(dayNumber, false, true)
-        assertEquals("42588603", Day7(real).answerTwo())
+        assertEquals("42588603", Day6(real).answerTwo())
     }
 
 
@@ -36,7 +36,7 @@ class Day6Test {
         val real = ReadUtil.readInputAsList(dayNumber, false, true)
 
         MarcoUtil.avgTime("answer 2", 10){
-            assertEquals("42588603", Day7(real).answerTwo())
+            assertEquals("42588603", Day6(real).answerTwo())
         }
     }
 }
