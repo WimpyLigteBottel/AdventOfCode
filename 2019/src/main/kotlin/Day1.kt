bec9aa0fa86561c9bcda77c57d66d9097dc4e8e6
mnel <epos.smurfmarco@gmail.com>
2024-11-04 16:24:46 +0100

    day1 2019

---
+++ 2019/src/main/kotlin/Day1.kt
@@ -0,0 +1,43 @@
+import java.nio.file.Files
+import java.nio.file.Path
+
+class Day1 {
+
+    fun part1(input: List<String>): String {
+
+        val answer = input
+            .map {
+                it.toLong().calculate()
+            }
+            .sum()
+
+        return answer.toString()
+    }
+
+    private fun Long.calculate() = ((this.toDouble() / 3) - 2).toLong()
+
+
+    private fun calculateRecursively(input: Long): Long {
+        if (input <= 3) {
+            return 0
+        }
+        val inputA = input.calculate()
+        val InputB = calculateRecursively(inputA)
+        return inputA + InputB
+    }
+
+
+    fun part2(input: List<String>): String {
+        return input
+            .sumOf { calculateRecursively(it.toLong()) }
+            .toString()
+    }
+}
+
+fun main() {
+    val lines = Files.readAllLines(Path.of("C:\\code\\AdventOfCode\\2019\\src\\main\\resources\\Day1"))
+
+    println("part 1 = ${Day1().part1(lines)}")
+    println("part 2 = ${Day1().part2(lines)}")
+
+}
---
+++ 2019/src/main/kotlin/Day1Test.kt
@@ -0,0 +1,19 @@
+import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.Test
+import java.nio.file.Files
+import java.nio.file.Path
+
+class Day1Test {
+
+    val input = Files.readAllLines(Path.of("C:\\code\\AdventOfCode\\2019\\src\\main\\resources\\Day1"))
+
+    @Test
+    fun part1() {
+        assertEquals("3474920",Day1().part1(input))
+    }
+
+    @Test
+    fun part2() {
+        assertEquals("5209504",Day1().part2(input))
+    }
+}
