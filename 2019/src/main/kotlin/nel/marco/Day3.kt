b293a3db6970aae1f1d2b2ba32963165945b706d
mnel <epos.smurfmarco@gmail.com>
2024-11-07 21:26:51 +0100

    Solved day 3 part 1 and part 2

--- 2019/src/main/kotlin/nel/marco/Day3.kt
+++ 2019/src/main/kotlin/nel/marco/Day3.kt
@@ -1,5 +1,6 @@
 package nel.marco
 
+import nel.marco.Day3.Point
 import java.nio.file.Files
 import java.nio.file.Path
 import java.nio.file.StandardOpenOption
@@ -9,47 +10,18 @@ class Day3(
     val fileName: String = "example"
 ) {
 
+    val day3Helper = Day3Helper(fileName)
+
     var cacheMap = mutableMapOf<Point, Pair<String, String>>()
-
-    fun printMap(map: MutableMap<Point, Pair<String, String>>) {
-        var maxX = map.maxBy { it.key.x }.key.x
-        var minX = map.minBy { it.key.x }.key.x
-        var maxY = map.maxBy { it.key.y }.key.y
-        var minY = map.minBy { it.key.y }.key.y
-
-        var sb = StringBuilder(100000)
-        val file = Path.of("C:\\Users\\eposm\\AppData\\Roaming\\JetBrains\\IdeaIC2024.2\\scratches\\$fileName")
-
-        Files.deleteIfExists(file)
-
-
-        for (x in minX..maxX + 1) {
-            for (y in minY..maxY + 1) {
-                val location = map[Point(x, y)]
-                location?.let {
-                    sb.append(it.second)
-                } ?: sb.append(" ")
-            }
-            sb.append(System.lineSeparator())
-        }
-
-        Files.writeString(
-            file,
-            sb.toString(),
-            StandardOpenOption.CREATE_NEW
-        )
-    }
 
     fun part1(input: List<String>): String {
         val map = mutableMapOf<Point, Pair<String, String>>()
         val wireA = input[0].split(",")
         val wireB = input[1].split(",")
 
-
         var lastLocation = Point(0, 0)
         map[lastLocation] = "A" to "o"
         wireA.forEach { input ->
-
             val direction = input.substring(0, 1) //D, U , R , L
             val amount = input.substring(1) // D[xxx]
 
@@ -72,7 +44,7 @@ class Day3(
             .map { manhattanDistance(Point(0, 0), it.key) } // Calculate the distance
             .sorted() // sort ascending
 
-        printMap(map)
+        day3Helper.printMap(map)
 
         return crossingPoints.first().toString()
     }
@@ -211,13 +183,11 @@ class Day3(
             val direction = input.substring(0, 1) //D, U , R , L
             val amount = input.substring(1) // D[xxx]
 
-            var exit = false
             runCatching {
                 lastLocation = process(direction, amount, lastLocation, map, search)
             }.onSuccess {
                 ACount = ACount + amount.toInt()
             }.onFailure {
-                exit = true
                 val exception = it as CountException
                 ACount += exception.count
                 return ACount
@@ -243,5 +213,37 @@ class Day3(
         fun left() = Point(x - 1, y)
 
         fun right() = Point(x + 1, y)
+    }
+}
+
+class Day3Helper(val fileName: String) {
+
+    fun printMap(map: MutableMap<Point, Pair<String, String>>) {
+        var maxX = map.maxBy { it.key.x }.key.x
+        var minX = map.minBy { it.key.x }.key.x
+        var maxY = map.maxBy { it.key.y }.key.y
+        var minY = map.minBy { it.key.y }.key.y
+
+        var sb = StringBuilder(100000)
+        val file = Path.of("C:\\Users\\eposm\\AppData\\Roaming\\JetBrains\\IdeaIC2024.2\\scratches\\$fileName")
+
+        Files.deleteIfExists(file)
+
+
+        for (x in minX..maxX + 1) {
+            for (y in minY..maxY + 1) {
+                val location = map[Point(x, y)]
+                location?.let {
+                    sb.append(it.second)
+                } ?: sb.append(" ")
+            }
+            sb.append(System.lineSeparator())
+        }
+
+        Files.writeString(
+            file,
+            sb.toString(),
+            StandardOpenOption.CREATE_NEW
+        )
     }
 }
--- 2019/src/test/kotlin/nel/marco/Day3Test.kt
+++ 2019/src/test/kotlin/nel/marco/Day3Test.kt
@@ -14,11 +14,6 @@ class Day3Test {
     val day = Day3()
 
     @Test
-    fun part1() {
-        assertEquals("X", day.part1(input))
-    }
-
-    @Test
     fun examples() {
         assertEquals("6", Day3("example1").part1(listOf("R8,U5,L5,D3", "U7,R6,D4,L4")))
         assertEquals("159", Day3("example2").part1(listOf("R75,D30,R83,U83,L12,D49,R71,U7,L72", "U62,R66,U55,R34,D71,R55,D58,R83")))
@@ -32,9 +27,13 @@ class Day3Test {
         assertEquals("410", Day3("example3").part2(listOf("R98,U47,R26,D63,R33,U87,L62,D20,R33,U53,R51","U98,R91,D20,R16,D67,R40,U7,R15,U6,R7")))
     }
 
+    @Test
+    fun part1() {
+        assertEquals("855", day.part1(input))
+    }
 
     @Test
     fun part2() {
-        assertEquals("X", day.part2(input))
+        assertEquals("11238", day.part2(input))
     }
 }
