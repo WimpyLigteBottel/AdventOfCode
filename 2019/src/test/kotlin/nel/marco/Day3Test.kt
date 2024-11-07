3ebee447bd81d5eabf4093594b66256c6678c169
mnel <epos.smurfmarco@gmail.com>
2024-11-07 21:23:07 +0100

    Solved day 3 part 1 and part 2

--- 2019/src/test/kotlin/nel/marco/Day3Test.kt
+++ 2019/src/test/kotlin/nel/marco/Day3Test.kt
@@ -1,8 +1,9 @@
 package nel.marco
 
+import org.junit.jupiter.api.Assertions.assertEquals
 import org.junit.jupiter.api.Test
-
-import org.junit.jupiter.api.Assertions.*
+import org.junit.jupiter.params.ParameterizedTest
+import org.junit.jupiter.params.provider.CsvSource
 import java.nio.file.Files
 import java.nio.file.Path
 
@@ -16,6 +17,21 @@ class Day3Test {
     fun part1() {
         assertEquals("X", day.part1(input))
     }
+
+    @Test
+    fun examples() {
+        assertEquals("6", Day3("example1").part1(listOf("R8,U5,L5,D3", "U7,R6,D4,L4")))
+        assertEquals("159", Day3("example2").part1(listOf("R75,D30,R83,U83,L12,D49,R71,U7,L72", "U62,R66,U55,R34,D71,R55,D58,R83")))
+        assertEquals("135", Day3("example3").part1(listOf("R98,U47,R26,D63,R33,U87,L62,D20,R33,U53,R51","U98,R91,D20,R16,D67,R40,U7,R15,U6,R7")))
+    }
+
+    @Test
+    fun examples_Part2() {
+        assertEquals("30", Day3("example1").part2(listOf("R8,U5,L5,D3", "U7,R6,D4,L4")))
+        assertEquals("610", Day3("example2").part2(listOf("R75,D30,R83,U83,L12,D49,R71,U7,L72", "U62,R66,U55,R34,D71,R55,D58,R83")))
+        assertEquals("410", Day3("example3").part2(listOf("R98,U47,R26,D63,R33,U87,L62,D20,R33,U53,R51","U98,R91,D20,R16,D67,R40,U7,R15,U6,R7")))
+    }
+
 
     @Test
     fun part2() {
