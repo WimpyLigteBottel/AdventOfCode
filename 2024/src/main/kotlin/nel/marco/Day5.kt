e08811f194252ef4516790d889101f8763b3749e
mnel <mnel@bol.com>
2024-12-04 15:26:47 +0100

    setting up day 5

---
+++ 2024/src/main/kotlin/nel/marco/Day5.kt
@@ -0,0 +1,14 @@
+package nel.marco
+
+
+class Day5(readInput: List<String>) : Day(readInput) {
+
+    override fun answerOne(): String {
+        return ""
+    }
+
+
+    override fun answerTwo(): String {
+       return  ""
+    }
+}
