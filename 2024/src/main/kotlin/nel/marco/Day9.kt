2e75b6a15f6762b931ef6355c8be0d6f1d79a30a
mnel <epos.smurfmarco@gmail.com>
2024-12-08 20:38:59 +0100

    setting up day 9

---
+++ 2024/src/main/kotlin/nel/marco/Day9.kt
@@ -0,0 +1,13 @@
+package nel.marco
+
+
+class Day9(readInput: List<String>) : Day(readInput) {
+
+    override fun answerOne(): String {
+       return ""
+    }
+
+    override fun answerTwo(): String {
+        return ""
+    }
+}
