85ce107edaafe8d40dd2081609cd97be0e5f34e3
mnel <mnel@bol.com>
2024-12-09 12:54:04 +0100

    day 9 changes

--- 2024/src/main/kotlin/nel/marco/Day9.kt
+++ 2024/src/main/kotlin/nel/marco/Day9.kt
@@ -1,47 +1,88 @@
 package nel.marco
 
+import java.math.BigInteger
+import java.util.Collections
+
 
 class Day9(useExample: Boolean = false, useMac: Boolean = false) : Day(9, useExample = useExample, macBook = useMac) {
 
     override fun answerOne(): String {
 
-        val map: Map<Int,String> = ('A'..'Z').mapIndexed { index, c ->
-            index + 10 to c.toString()
-        }.associate { it.first to it.second }
+        val mapList = 100000
+        val map: MutableMap<Int, String> = (0..mapList)
+            .mapIndexed { index, c -> index to c.toString() }
+            .associate { it.first to it.second +"."}
+            .toMutableMap()
 
+        val reverse: MutableMap<String, Int> = (0..mapList)
+            .mapIndexed { index, c -> index to c.toString() }
+            .associate { it.second+"." to it.first }
+            .toMutableMap()
+
+
+        var newList = expandList(map)
+//        println("before: " + newList)
+        compact(newList)
+//        println("after: " + newList)
+
+
+        var total = newList
+            .filter { it != "" }
+            .mapIndexed { index: Int, indexedValue: String ->
+                val index = BigInteger("$index")
+                val other = BigInteger("${reverse[indexedValue]!!}")
+
+                index.times(other)
+            }.fold(BigInteger.ZERO) { acc, bigInteger -> acc.add(bigInteger) }
+
+        return "$total"
+    }
+
+    private fun expandList(map: MutableMap<Int, String>): MutableList<String> {
         val line = readInput.first().map { it }
 
-
-        var digitsList = mutableListOf<String>()
-        var spaceList = mutableListOf<String>()
+        var newList = mutableListOf<String>()
 
         var index = 0
         var digit = 0
-        while (index <= line.size - 2) {
+        while (index < line.size - 1) {
             var file = line[index].digitToInt()
             var space = line[index + 1].digitToInt()
             repeat(file) {
-                print(map[digit] ?: digit)
+                newList.add(map[digit].toString())
             }
             repeat(space) {
-                print(".")
+                newList.add(".")
             }
             digit++
             index++
+            index++
         }
-
-        return ""
+        // last line
+        repeat(line[index].digitToInt()) {
+            newList.add(map[digit].toString())
+        }
+        return newList
     }
 
 
-    fun List<String>.packInto(packedInto: MutableList<String>) {
-        this.asReversed().forEach { numberToPack ->
-            for ((index, x) in packedInto.withIndex()) {
-                if (packedInto[index] == ".") {
-                    packedInto[index] = numberToPack
+    fun compact(packedInto: MutableList<String>) {
+        val withoutDots = packedInto.filter { it != "." }.asReversed().toMutableList()
+
+        var counter = 0
+
+        for ((index, s) in packedInto.withIndex()) {
+            if (s == ".") {
+                counter++
+                while (packedInto[packedInto.size - counter] == ".") {
+                    counter++
                 }
+                Collections.swap(packedInto, index, packedInto.size - counter)
+                packedInto.set(packedInto.size - counter, "")
             }
         }
+
+        packedInto.removeAll { it == "" }
     }
 
     override fun answerTwo(): String {
--- 2024/src/test/nel/marco/Day9Test.kt
+++ 2024/src/test/nel/marco/Day9Test.kt
@@ -1,6 +1,7 @@
 package nel.marco
 
 import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.Assertions.assertNotEquals
 import org.junit.jupiter.api.Test
 
 class Day9Test {
@@ -8,11 +9,11 @@ class Day9Test {
     val day = Day9(useExample = false, useMac = true)
     val example = Day9(useExample = true, useMac = true)
 
-
     @Test
     fun answerOne() {
-        assertEquals("X", example.answerOne())
-        assertEquals("X", day.answerOne())
+        assertEquals("1928", example.answerOne())
+        assertNotEquals("6273684533700", day.answerOne()) // not 6273684533700
+        assertEquals("X", day.answerOne()) // not 6273684533700
     }
 
     @Test
@@ -31,3 +32,4 @@ class Day9Test {
         }
     }
 }
+
