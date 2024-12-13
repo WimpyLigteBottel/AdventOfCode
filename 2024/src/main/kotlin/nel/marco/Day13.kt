7ac5842cc6c11f505c46a3209f556d4fa36ffece
mnel <epos.smurfmarco@gmail.com>
2024-12-13 22:32:22 +0100

    setup day 13

---
+++ 2024/src/main/kotlin/nel/marco/Day13.kt
@@ -0,0 +1,15 @@
+package nel.marco
+
+
+class Day13(useExample: Boolean = false, useMac: Boolean = false) :
+    Day(dayNumber = 13, useExample = useExample, macBook = useMac) {
+
+    override fun answerOne(): String {
+        TODO()
+
+    }
+
+    override fun answerTwo(): String {
+        TODO()
+    }
+}
---
+++ 2024/src/test/nel/marco/Day13Test.kt
@@ -0,0 +1,36 @@
+package nel.marco
+
+import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.Disabled
+import org.junit.jupiter.api.Test
+
+class Day13Test {
+
+    val day = Day13(useExample = false, useMac = false)
+    val example = Day13(useExample = true, useMac = false)
+
+    @Test
+    fun answerOne() {
+        assertEquals("11", example.answerOne())
+        assertEquals("X", day.answerOne())
+    }
+
+
+    @Test
+    fun answerTwo() {
+        assertEquals("X", example.answerTwo())
+        assertEquals("X", day.answerTwo())
+    }
+
+
+    @Test
+    @Disabled
+    fun avgTime() {
+        MarcoUtil.avgTime("part1") {
+            assertEquals("X", day.answerOne())
+        }
+        MarcoUtil.avgTime("part2") {
+            assertEquals("X", day.answerTwo())
+        }
+    }
+}
