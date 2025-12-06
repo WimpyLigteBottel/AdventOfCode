72d87f26345b002224f68afaa32c1111d3906bbd
mnel <epos.smurfmarco@gmail.com>
2025-12-07 00:00:28 +0100

    optimizing rust day 6 part 2

--- rust-2025/src/day6/day6.rs
+++ rust-2025/src/day6/day6.rs
@@ -9,9 +9,9 @@ pub(crate) fn part1(result: &[String]) -> String {
 
     let mut total: i128 = 0;
 
-    for x in 0..operator.len()-1 {
+    for x in 0..operator.len() - 1 {
         let current = operator[x].index;
-        let next = operator[x + 1].index;   // <-- FIXED
+        let next = operator[x + 1].index; // <-- FIXED
 
         let new_list = [
             result[0][current..next].trim().parse::<i128>().unwrap(),
@@ -25,7 +25,6 @@ pub(crate) fn part1(result: &[String]) -> String {
 
     total.to_string()
 }
-
 
 fn calculate_answer(operator: char, new_list: Vec<i128>) -> i128 {
     let mut answer = 0;
@@ -54,45 +53,31 @@ fn get_operators_and_starting_index(result: &[String]) -> Vec<OperatorAndIndex> 
         }
     }
 
-     operators
+    operators
 }
 
-//part 2 took 0.034 ms average (totalRuns=146536; inSeconds=5)
+// part 2 took 0.373 ms average (totalRuns=13406; inSeconds=5)
 pub(crate) fn part2(result: &[String]) -> String {
-    let mut row1: Vec<String> = Vec::new();
-    let mut row2: Vec<String> = Vec::new();
-    let mut row3: Vec<String> = Vec::new();
-    let mut row4: Vec<String> = Vec::new();
-
     let mut operator = get_operators_and_starting_index(&result);
-
-    for x in 0..operator.len() - 1 {
-        let current = operator[x].index;
-        let next = operator[x + 1].index - 1;
-
-        row1.push(result[0][current..next].to_string());
-        row2.push(result[1][current..next].to_string());
-        row3.push(result[2][current..next].to_string());
-        row4.push(result[3][current..next].to_string());
-    }
-
-    // removed the extra operator to make my life easier
-    operator.pop();
 
     let mut total: i128 = 0;
 
-    for x in 0..operator.len() {
-        let new_list: Vec<i128> = (0..row1[x].len())
-            .map(|y| {
-                let one = &row1[x][y..y + 1];
-                let two = &row2[x][y..y + 1];
-                let three = &row3[x][y..y + 1];
-                let four = &row4[x][y..y + 1];
+    for x in 0..operator.len() - 1 {
+        let current = operator[x].index;
+        let next = operator[x + 1].index; // <-- FIXED
 
-                let new = [one, two, three, four]
+        let new_list: Vec<i128> = (0..next - current - 1)
+            .map(|y| {
+                let one = &result[0][current..next][y..y + 1].trim();
+                let two = &result[1][current..next][y..y + 1].trim();
+                let three = &result[2][current..next][y..y + 1].trim();
+                let four = &result[3][current..next][y..y + 1].trim();
+
+                let new = [*one, *two, *three, *four]
                     .join("")
                     .replace(" ", "")
                     .to_string();
+
                 new.parse::<i128>().unwrap()
             })
             .collect();
